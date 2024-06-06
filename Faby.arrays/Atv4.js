ef2f5e37465f301079d33b330d3acd81b68be542
Faby4023 <143666861+Faby4023@users.noreply.github.com>
2024-06-05 21:14:00 -0300

    Add files via upload

--- Faby.arrays/Atv4.js
+++ Faby.arrays/Atv4.js
@@ -1,2 +1,7 @@
-var idades = []
-var nomes = []
+entrada = require("readline-sync");
+var nomes = ["fabyanna","jaqueline","ana","carol","aurora"]
+var idades = [41,46,45,35,23]
+for(var i = 0; i< 5; i++){
+
+    console.log("O nome do usuário é: " + nomes[i], "tem", idades[i], "anos");
+}
---
+++ Faby.arrays/Atv5.js
@@ -0,0 +1,7 @@
+entrada = require("readline-sync");
+
+var idades = parseInt(entrada.question);
+for(var i = 0; i< 5; i++){
+    
+    console.log("Digite a idade: ");
+}
